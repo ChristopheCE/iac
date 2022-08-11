fdbc2f960836193a9466c62835bbb2edb1fabd96
Christophe Leybaert <christophe.leybaert@cegeka.com>
2022-08-11 11:17:21 +0200

    Fix

--- main.tf
+++ main.tf
@@ -22,9 +22,8 @@ resource "azurerm_resource_group" "test" {
 // Create storage account
 resource "azurerm_storage_account" "test" {
   name                            = "sttest${var.environment_code}"
-  resource_group_name             = var.resource_group_name
-  location                        = var.location
+  resource_group_name             = azurerm_resource_group.test.name
+  location                        = azurerm_resource_group.test.location
   account_tier                    = "Standard"
   account_replication_type        = "LRS"
-  tags                            = var.tags
 }
