cfae209e88cef61027c700180bfacf2d7516bd53
Christophe Leybaert <christophe.leybaert@cegeka.com>
2022-08-24 10:44:47 +0200

    Sonar added

--- main.tf
+++ main.tf
@@ -41,3 +41,32 @@ resource "azurerm_container_registry" "test" {
   location            = azurerm_resource_group.test.location
   sku                 = "Standard"
 }
+
+// Create sonar  resource group
+resource "azurerm_resource_group" "sonar" {
+  name     = "rg-sonar-${var.environment_code}"
+  location = "westeurope"
+}
+
+// Creat App Service Plan
+resource "azurerm_service_plan" "sonar" {
+  name = "plan-sonar-${var.environment_code}"
+  resource_group_name = azurerm_resource_group.sonar.name
+  location            = azurerm_resource_group.sonar.location
+  os_type             = "Linux"
+  sku_name            = "F1"
+}
+
+resource "azurerm_linux_web_app" "sonar" {
+  name                = "app-sonar-${var.environment_code}"
+  resource_group_name = azurerm_resource_group.sonar.name
+  location            = azurerm_service_plan.sonar.location
+  service_plan_id     = azurerm_service_plan.sonar.id
+
+  site_config {
+    application_stack {
+      docker_image = "sonarqube"
+      tdocker_image_tag = "9.6-community"    
+    }
+  }
+}
